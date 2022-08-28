f21a7b817992847af866162fb5c2dbb0d1e1a1ad
free-ko <duddnr654@naver.com>
2022-08-28 20:34:45 +0900

    [FEAT] 3주차 1번 제출

--- 3week/assignment/01.ts
+++ 3week/assignment/01.ts
@@ -4,5 +4,9 @@
  * 파라미터에 필요한 source 와 target 을 좁혀주세요.
  * @description: 링크: https://developers.naver.com/docs/papago/papago-nmt-api-reference.md
  * */
-type PapagoParamsSource = string;
-type PapagoParamsTarget = string;
+
+{
+    type Language = 'ko' | 'en' | 'ja' | 'zh-CN' | 'zh-TW' | 'vi' | 'id' | 'th' | 'de' | 'ru' | 'es' | 'it' | 'fr';
+    type PapagoParamsSource = Language;
+    type PapagoParamsTarget = Language;
+}
